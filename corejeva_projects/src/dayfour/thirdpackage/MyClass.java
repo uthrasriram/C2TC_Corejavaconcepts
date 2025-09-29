1639b55631108a8fb37da97b518033f254894f5c
uthrasriram <uthrasriram380@gmail.com>
2025-09-29 21:29:10 +0530

    commit

---
+++ corejeva_projects/src/dayfour/thirdpackage/MyClass.java
@@ -0,0 +1,37 @@
+package dayfour.thirdpackage;
+
+public class MyClass {
+
+		// TODO Auto-generated method stub
+			private static MyClass obj=new MyClass();
+			private int id;
+			
+			public int getId()
+			{
+				return id;
+			}
+			
+			public void setId(int id)
+			{
+				this.id=id;
+			}
+			
+			private MyClass() {
+				System.out.println("MyClass object created");		
+			}
+			
+			public static MyClass getObject() //factory method
+			{
+				return obj;
+			}
+
+			@Override
+			public String toString() {
+				return "MyClass [id=" + id + "]";
+			}
+			
+		
+
+	}
+
+
---
+++ corejeva_projects/src/dayfour/thirdpackage/PrivateConstructorDemo.java
@@ -0,0 +1,20 @@
+package dayfour.thirdpackage;
+
+public class PrivateConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
+}
+
+	
+
+
